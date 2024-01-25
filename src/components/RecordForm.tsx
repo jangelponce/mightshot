d292a07dc5032c051c6349370ad5ae9830f1d5fc
Angel Ponce <angel.ponce@doctible.com>
2024-01-24 17:50:59 -0700

    Add shadcn and dependencies

--- src/components/RecordForm.tsx
+++ src/components/RecordForm.tsx
@@ -34,29 +34,35 @@ const initRecorder = async () => {
   return recorder
 }
 
-export default function Recorder () {
+export default function RecordForm () {
   const [open, setOpen] = useState(false)
-  // const [_, setRecorder] = useState(null)
+  // eslint-disable-next-line no-undef
+  const [recorder, setRecorder] = useState < any > (null)
 
   useEffect(() => {
     if (open) {
       // Start recording
       console.log('Start recording')
-      initRecorder()
+      initRecorder().then((rec) => {
+        setRecorder(rec)
+      })
     } else {
+      recorder?.stop()
       // Stop recording
     }
   }, [open])
 
   return (
-    <button
-      onClick={() => setOpen(!open)}
-      className='relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'
-    >
-      <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
-      <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
-        🎥 Start recording
-      </span>
-    </button>
+    <div className='p-2'>
+      <button
+        onClick={() => setOpen(!open)}
+        className='relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50'
+      >
+        <span className='absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]' />
+        <span className='inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl'>
+          {open ? '🛑 Stop recording' : '🎥 Start recording'}
+        </span>
+      </button>
+    </div>
   )
 }
